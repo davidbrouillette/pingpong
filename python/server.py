d741dabb528097e58e8803fbdc7552f74b26fc72
davidbrouillette <dbroumusic@gmail.com>
2018-08-15 10:56:15 -0700

    Added ideas readme and moved files

--- python/server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def serveUI():
-    return 
--- src/server/player.model.js
+++
@@ -1,7 +0,0 @@
-
-const Player = {
-    name: "",
-    wins: 0,
-    loses: 0
-}
-module.exports = Player;
